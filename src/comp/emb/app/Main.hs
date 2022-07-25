b07dd88ee1660c55b17b266e0fac9035ba9a4880
Bruno Flores <bruno@brunoflores.com.br>
2022-07-26 00:00:48 +1000

    Parse a bsv file.

--- src/comp/emb/app/Main.hs
+++ src/comp/emb/app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import CSyntax
+import Depend
+import Error
+import FileIOUtil
+import FlagsDecode
+import Id
+import Position
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let fname = "/home/bruno/devel/bsc/src/comp/examples/FibOne.bsv"
+  errh <- initErrorHandle
+  file <-
+    readFileCatch
+      errh
+      noPosition
+      fname
+  pkg <-
+    parseSrc
+      False
+      errh
+      (defaultFlags "")
+      True
+      fname
+      file
+  print (show pkg)
